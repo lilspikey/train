7c37324a607a3ecc4bba0d4ddc0fa00d85f94eed
John Montgomery <john@littlespikeyland.com>
2014-04-15 19:27:07 +0100

    Skeleton js

---
+++ static/js/train.js
@@ -0,0 +1,8 @@
+(function(){
+    // TODO scale width height to full windows size
+    var layout = Raphael('layout', 640, 480);
+
+    layout.circle(300,200, 50).
+        attr("fill", "#f00");
+    
+})();
