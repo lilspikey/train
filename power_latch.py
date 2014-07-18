b05d9d13adf5891a94fd02a2b830f4095ac77b67
John Montgomery <john@littlespikeyland.com>
2014-07-18 19:55:58 +0000

    Initial script for interfacing with power button
    Doesn't actually shutdown RPi, just prints out message and puts confirm
    pin low.

---
+++ power_latch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''
+simple script to listen out for changes in GPIO port that show that
+power button pressed and we should shutdown RPi
+'''
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+SHUTDOWN_REQUEST_PIN = 16
+SHUTDOWN_CONFIRM_PIN = 18
+
+try:
+    GPIO.setup(SHUTDOWN_REQUEST_PIN, GPIO.IN)
+    
+    GPIO.setup(SHUTDOWN_CONFIRM_PIN, GPIO.OUT, initial=GPIO.HIGH)
+
+    while GPIO.input(SHUTDOWN_REQUEST_PIN) == GPIO.HIGH:
+        time.sleep(0.25)
+
+    print("SHUTDOWN")
+    GPIO.output(SHUTDOWN_CONFIRM_PIN, GPIO.LOW)
+finally:
+    # only cleanup request pin, as we want confirm pin to stay high
+    # until RPi has actually shutdown
+    GPIO.cleanup(SHUTDOWN_REQUEST_PIN)
+
+
